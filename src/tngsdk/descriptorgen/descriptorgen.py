a02964827f73643cf5c86c964fef5a67e1856cfb
Stefan Schneider <stefan.schneider@upb.de>
2018-06-21 14:39:49 +0200

    implemented CLI descriptorgen of tango descriptors

--- src/tngsdk/descriptorgen/descriptorgen.py
+++ src/tngsdk/descriptorgen/descriptorgen.py
@@ -1,5 +1,5 @@
 import argparse
-import yaml
+import oyaml as yaml        # ordered yaml to avoid reordering of descriptors
 import copy
 from os import path
 
@@ -47,8 +47,16 @@ def generate_tango():
     nsd['name'] = args.name
     nsd['description'] = args.description
 
+    # keep reusable parts of the default NSD that are independent from #VNFs
+    # eg, there is always a mgmt vLink and a vLink from the input for vnf0
+    # remove other stuff which will be replaced (vnf0-2-output vLink)
+    del nsd['virtual_links'][2]
+    del nsd['forwarding_graphs'][0]['constituent_virtual_links'][1]
+    del nsd['forwarding_graphs'][0]['network_forwarding_paths'][0]['connection_points'][2:4]
+
+    # now updated and extend the NSD
     for i, vnf in enumerate(vnfds):
-        # list involved VNFs
+        # list of involved VNFs
         # first entry already exists -> adjust, then append new ones
         if i > 0:
             nsd['network_functions'].append({})
@@ -57,7 +65,58 @@ def generate_tango():
         nsd['network_functions'][i]['vnf_vendor'] = vnf['vendor']
         nsd['network_functions'][i]['vnf_version'] = vnf['version']
 
-        # TODO: vLinks, forwarding graph (see javascript implementation)
+        # create corresponding vLinks
+        # add vLink to next vnf
+        if i < len(vnfds) - 1:
+            nsd['virtual_links'].append({
+                'id': 'vnf{}-2-vnf{}'.format(i, i+1),
+                'connectivity_type': 'E-Line',
+                'connection_points_reference': [
+                    'vnf{}:output'.format(i), 'vnf{}:input'.format(i+1)
+                ]
+            })
+        # for last vnf in chain, set vLink to output instead
+        else:
+            nsd['virtual_links'].append({
+                'id': 'vnf{}-2-output'.format(i, i+1),
+                'connectivity_type': 'E-Line',
+                'connection_points_reference': [
+                    'vnf{}:output'.format(i), 'output'
+                ]
+            })
+
+        # add mgmt connection point (already exists for vnf0)
+        if i > 0:
+            nsd['virtual_links'][0]['connection_points_reference'].append(
+                'vnf{}:mgmt'.format(i)
+            )
+
+    # adjust forwarding graph
+    nsd['forwarding_graphs'][0]['number_of_virtual_links'] = len(vnfds) + 1
+    # append new vLinks (skip mgmt and input-2-vnf0, which are already there)
+    for i in range(2, len(nsd['virtual_links'])):
+        nsd['forwarding_graphs'][0]['constituent_virtual_links'].append(
+            nsd['virtual_links'][i]['id']
+        )
+    # append new vnfs
+    for i in range(1, len(vnfds)):
+        nsd['forwarding_graphs'][0]['constituent_vnfs'].append(
+            nsd['network_functions'][i]['vnf_id']
+        )
+
+    # append in- and output of each vLink (keep input, vnf0:input, vnf0:output)
+    pos = 3
+    for i in range(2, len(nsd['virtual_links'])):
+        nsd['forwarding_graphs'][0]['network_forwarding_paths'][0]['connection_points'].append({
+            'connection_point_ref': nsd['virtual_links'][i]['connection_points_reference'][0],
+            'position': pos
+        })
+        pos += 1
+        nsd['forwarding_graphs'][0]['network_forwarding_paths'][0]['connection_points'].append({
+            'connection_point_ref': nsd['virtual_links'][i]['connection_points_reference'][1],
+            'position': pos
+        })
+        pos += 1
 
     return nsd, vnfds
 
