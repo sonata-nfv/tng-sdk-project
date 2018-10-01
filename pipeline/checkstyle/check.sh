3647b62b1cf748cac672c1535d524b48c9b50cec
Stefan Schneider <stefan.schneider@upb.de>
2018-10-01 17:48:24 +0200

    added config for pycodestyle

--- pipeline/checkstyle/check.sh
+++ pipeline/checkstyle/check.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 echo "checking style ..."
-docker run -i --rm registry.sonata-nfv.eu:5000/tng-sdk-project pycodestyle --exclude .eggs --max-line-length=120 .
+docker run -i --rm registry.sonata-nfv.eu:5000/tng-sdk-project pycodestyle
 echo "done."
-# always exit with 0 (ugly code style is not an error :))
-#exit 0
