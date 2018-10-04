2ccbf4b83b26ecf2bccba78a302485c1f6f7010f
Stefan Schneider <stefan.schneider@upb.de>
2018-10-04 18:12:59 +0200

    fixed unittests: first start docker in service mode, then run tests

--- pipeline/unittest/test.sh
+++ pipeline/unittest/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-#docker run -i --rm registry.sonata-nfv.eu:5000/tng-sdk-project tng-project -s
-docker run -i --rm registry.sonata-nfv.eu:5000/tng-sdk-project pytest --tavern-beta-new-traceback
+docker run --rm -d -p 5098:5098 --name tng-sdk-project registry.sonata-nfv.eu:5000/tng-sdk-project
+docker exec -i tng-sdk-project pytest --tavern-beta-new-traceback
+docker stop tng-sdk-project
