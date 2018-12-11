9cab97811cd41210c6e513c94b338d946e11a5b3
peusterm <manuel.peuster@uni-paderborn.de>
2018-12-11 08:58:05 +0100

    Fix: Typo in script
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- pipeline/unittest/test.sh
+++ pipeline/unittest/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # esnure that no old containe is there
-docker rm -f tng-sdk-project || True
+docker rm -f tng-sdk-project || true
 # start container
 docker run --rm -d -p 5098:5098 --name tng-sdk-project registry.sonata-nfv.eu:5000/tng-sdk-project
 # trigger unittests
