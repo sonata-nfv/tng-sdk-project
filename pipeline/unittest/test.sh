cd461e1c63739422e45e696936242ba79e7a3aa7
Zulfiqar Ahmed <zulfiqarjunejo@live.com>
2019-06-12 10:21:23 +0200

    (tng-project): added another experiment to pass pipeline

--- pipeline/unittest/test.sh
+++ pipeline/unittest/test.sh
@@ -5,6 +5,7 @@ docker rm -f tng-sdk-project || true
 # start container
 docker run --rm -d -p 5098:5098 --name tng-sdk-project registry.sonata-nfv.eu:5000/tng-sdk-project
 # trigger unittests
-docker exec -i tng-sdk-project pytest --tavern-beta-new-traceback
+docker exec -i tng-sdk-project pytest 
+#--tavern-beta-new-traceback
 # kill the container
 docker rm -f tng-sdk-project
