b1c58769a987ba2b71343a703bb71c780587a982
Stefan Schneider <stefan.schneider@upb.de>
2019-08-09 15:54:28 +0200

    push to latest on dockerhub

--- pipeline/publish/publish.sh
+++ pipeline/publish/publish.sh
@@ -5,4 +5,4 @@ docker push registry.sonata-nfv.eu:5000/tng-sdk-project
 # publish to dev, which is then pushed to Docker Hub
 echo 'Tagging and pushing :dev'
 docker tag registry.sonata-nfv.eu:5000/tng-sdk-project:latest sonatanfv/tng-sdk-project:dev
-docker push sonatanfv/tng-sdk-project:dev
+docker push sonatanfv/tng-sdk-project:latest
